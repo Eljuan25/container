f2721051e3a0b68dbb42f6fbe43dafe77ea41ac4
juan25 <elbartoy@gmail.com>
2024-12-14 18:32:41 -0600

    Esto son los cambios para que sirva correcta mente

--- scripts.js
+++ scripts.js
@@ -1,7 +1,10 @@
-const container = document.getElementById("container");
-const incrementTBt = document.getElementById("incrementTBt");
-    let count = 0;
-    incrementTBt.addEventListener("click", () => {
-        count++;
-        container.textContent = count;
-    });
+const container = document.getElementById("container"); // Párrafo que muestra el número
+const incrementTBt = document.getElementById("incrementTBt"); // Botón de incremento
+
+let count = 0; // Variable para llevar el conteo
+
+// Añadir evento al botón
+incrementTBt.addEventListener("click", () => {
+    count++; // Incrementar el contador
+    container.textContent = count; // Actualizar el contenido del párrafo
+});
